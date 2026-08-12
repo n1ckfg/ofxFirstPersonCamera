3a6cc88ca6018fe5b8f8fae08a7bb865b78fb1e0
Nick Fox-Gieg <nick@fox-gieg.com>
2026-08-11 22:16:50 -0400

    begin update of 0.12.1

---
+++ example/addons.make
@@ -0,0 +1,2 @@
+ofxFirstPersonCamera
+ofxXmlSettings
---
+++ example/src/ofApp.h
@@ -0,0 +1,25 @@
+#include "ofMain.h"
+#include "ofxFirstPersonCamera.h"
+
+class ofApp : public ofBaseApp {
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+
+		ofxFirstPersonCamera camera;
+
+		ofCylinderPrimitive cylinders [50];
+
+};
+
+
